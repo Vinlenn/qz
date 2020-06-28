64f3630d9c0f28804f20a5b8b6c70ebeae4e1b11
Vinlenn <zzhengaiqiang@163.com>
2020-06-28 15:53:36 +0800

    支持json格式添加bean

--- src/java/com/vinlen/zxc/Demo.java
+++ src/java/com/vinlen/zxc/Demo.java
@@ -1,13 +1,12 @@
 package com.vinlen.zxc;
 
-import com.vinlen.zxc.controller.AnimalController;
 import com.vinlen.zxc.controller.UserController;
 import com.vinlen.zxc.ioc.annotationLoader.AnnoLoader;
 import com.vinlen.zxc.ioc.comboLoader.ComboLoader;
 
 
 public class Demo {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		new ComboLoader("ioc","com.vinlen.zxc");
 		UserController userController = (UserController) AnnoLoader.applicationContext.get(UserController.class);
 		userController.say();
