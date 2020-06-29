087d43615d39f8ee6ed521d3ae7fcf865305b95b
Vinlenn <zzhengaiqiang@163.com>
2020-06-29 17:47:24 +0800

    添加工具类

--- src/java/com/vinlen/zxc/Util/IocFactory.java
+++ src/java/com/vinlen/zxc/Util/IocFactory.java
@@ -10,4 +10,20 @@ public class IocFactory extends LinkedHashMap<Class<?>, Object> {
 	public <T> T get2Class(Class<T> type) {
 		return (T) get(type);
 	}
+
+	/**
+	 * 设置一个字段，如果值为 null 则表示移除
+	 *
+	 * @param clazz
+	 *            键
+	 * @param v
+	 *            值
+	 */
+	public void setOrRemove(Class<?> clazz, Object v) {
+		if (null == v) {
+			this.remove(clazz);
+		} else {
+			this.put(clazz, v);
+		}
+	}
 }
